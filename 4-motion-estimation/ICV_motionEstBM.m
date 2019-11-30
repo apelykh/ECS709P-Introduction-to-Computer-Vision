016b49975271872a61160076a277d55456b7b67d
Anton Pelykh <anton.pelykh@gmail.com>
2019-11-30 01:19:17 +0000

    Motion estimation works fully

--- 4-motion-estimation/ICV_motionEstBM.m
+++ 4-motion-estimation/ICV_motionEstBM.m
@@ -1,48 +1,80 @@
 function [motion_field, predicted_frame] = ICV_motionEstBM(prev_frame, ...
                                         cur_frame, block_size, window_size)
     arguments
-        prev_frame (:, :) uint8
-        cur_frame (:, :) uint8 {mustBeEqualSize(prev_frame, cur_frame)}
+        prev_frame (:, :, :) uint8
+        cur_frame (:, :, :) uint8 {mustBeEqualSize(prev_frame, cur_frame)}
         block_size uint16 {mustBeGreaterThan(block_size, 0), ...
                            mustBeEven(block_size), ...
                            mustEvenlyDivideSize(block_size, cur_frame)}
         window_size uint16 {mustBeGreaterThanOrEqual(window_size, block_size)}
     end
     
+    if size(prev_frame, 3) == 3
+        prev_frame_gray = ICV_rgb2gray(prev_frame, false);
+        cur_frame_gray = ICV_rgb2gray(cur_frame, false);
+    elseif size(prev_frame, 3) == 1
+        prev_frame_gray = prev_frame;
+        cur_frame_gray = cur_frame;
+    end
+    
     % store the value in a separate variable for convenience
     bh = block_size / 2;
-    
-    % populte arrays with x and y coordinates of block centers
-    [xc_coords, yc_coords] = meshgrid(bh:block_size:size(cur_frame, 2) - bh, ...
-                                      bh:block_size:size(cur_frame, 1) - bh);
-    x_len = double(zeros(size(xc_coords)));
-    y_len = double(zeros(size(yc_coords)));
 
+    mf_height = size(prev_frame_gray, 1) / block_size;
+    mf_width = size(prev_frame_gray, 2) / block_size;
+    motion_field = zeros(4, mf_height, mf_width);
+    
+    % x and y coordinates of motion vector centers
+    [motion_field(1, :, :), motion_field(2, :, :)] = ...
+        meshgrid(bh:block_size:size(cur_frame_gray, 2) - bh, ...
+                 bh:block_size:size(cur_frame_gray, 1) - bh);
+    % x length components of motion vectors
+    motion_field(3, :, :) = double(zeros(mf_height, mf_width));
+    % y length components of motion vectors
+    motion_field(4, :, :) = double(zeros(mf_height, mf_width));
+    
     % iterate through all the block centers
-    for ci = bh:block_size:size(prev_frame, 1) - bh
-        for cj = bh:block_size:size(prev_frame, 2) - bh
-            block = prev_frame((ci - bh + 1):ci + bh, ...
-                               (cj - bh + 1):cj + bh);
+    for ci = bh:block_size:size(prev_frame_gray, 1) - bh
+        for cj = bh:block_size:size(prev_frame_gray, 2) - bh
+            block = prev_frame_gray((ci - bh + 1):ci + bh, ...
+                                    (cj - bh + 1):cj + bh);
             [match_ci, match_cj] = ICV_matchBlock(block, [ci, cj], ...
-                                                  cur_frame, window_size);
-            
+                                                  cur_frame_gray, window_size);
             % indices of the curret block
             block_i = ceil(ci / block_size);
             block_j = ceil(cj / block_size);
             % x and y vector length components are corresponding
             % displacements between current and matched blocks
-            y_len(block_i, block_j) = double(match_ci) - double(ci);
-            x_len(block_i, block_j) = double(match_cj) - double(cj);
+            motion_field(3, block_i, block_j) = double(match_cj) - double(cj);
+            motion_field(4, block_i, block_j) = double(match_ci) - double(ci);
         end
     end
-    
-    motion_field = [xc_coords, yc_coords, x_len, y_len];
-    predicted_frame = cur_frame;
+    predicted_frame = ICV_predictFrame(prev_frame, motion_field, block_size);
+end
 
-%     imshow(cur_frame);
-%     hold on
-%     quiver(xc_coords, yc_coords, x_len, y_len, 1);
-%     hold off
+
+function predicted_frame = ICV_predictFrame(prev_frame, motion_field, block_size)
+    bh = block_size / 2;
+    predicted_frame = prev_frame;
+    
+    for block_i = 1:size(motion_field, 2)
+        for block_j = 1:size(motion_field, 3)
+            x_len = motion_field(3, block_i, block_j);
+            y_len = motion_field(4, block_i, block_j);
+            
+            if x_len == 0 || y_len == 0
+                continue
+            end
+            
+            cx = motion_field(1, block_i, block_j);
+            cy = motion_field(2, block_i, block_j);
+            block = prev_frame((cy - bh + 1):cy + bh, (cx - bh + 1):cx + bh, :);
+            
+            t_cx = cx + x_len;
+            t_cy = cy + y_len;
+            predicted_frame((t_cy - bh + 1):t_cy + bh, (t_cx - bh + 1):t_cx + bh, :) = block;
+        end
+    end
 end
 
 
@@ -100,6 +132,7 @@ function error = ICV_computeError(ref_block, block)
     num_elem = size(ref_block, 1) * size(ref_block, 2);
     error = sum(diff, 'all') / num_elem;
 end
+
 
 % Custom validation function
 function mustBeEqualSize(a, b)
--- 4-motion-estimation/motion_estimation.m
+++ 4-motion-estimation/motion_estimation.m
@@ -1,48 +1,48 @@
 video_file = './DatasetC.mpg';
+save_folder = './frames';
+save_figures = 0;
+block_size = 8;
+search_window = 16;
 % -------------------------------------------------------------------------
-
+ICV_create_folder(save_folder);
 v = VideoReader(video_file);
 prev_frame = NaN;
-
-% -------------------
-img1 = rgb2gray(imread('face-1.jpg'));
-img2 = rgb2gray(imread('face-1-shifted.jpg'));
-
-block_size = 9;
-hbm = vision.BlockMatcher('ReferenceFrameSource', 'Input port','BlockSize',[block_size block_size]);
-hbm.OutputValue = 'Horizontal and vertical components in complex form';
-halphablend = vision.AlphaBlender;
-% -------------------
-
-% ICV_motionEstBM(img1, img2, 16, 20);
 
 i = 1;
 while(hasFrame(v))
     cur_frame = readFrame(v);
-    cur_frame = rgb2gray(cur_frame);
-    if ~isnan(prev_frame)
-        [motion_field, predicted_frame] = ICV_motionEstBM(prev_frame, cur_frame, 8, 16);
-        
-        imshow(cur_frame);
-        hold on
-%         quiver(xc_coords, yc_coords, x_len, y_len, 1);
-        quiver(motion_field(1), motion_field(2), motion_field(3), motion_field(4), 1);
-        hold off
-        
-%         [motionVect, EScomputations] = motionEstES(prev_frame, cur_frame, 16, 20);
-
-%         motion = hbm(prev_frame, cur_frame);
-%         img12 = halphablend(prev_frame, cur_frame);
-% 
-%         [X,Y] = meshgrid(1:block_size:size(prev_frame, 2), 1:block_size:size(cur_frame, 1));    
-% 
-%         imshow(img12)
-%         hold on
-%         quiver(X(:), Y(:),real(motion(:)), imag(motion(:)), 0)
-%         hold off
-        break
+%     cur_frame = ICV_rgb2gray(cur_frame, false);
+    
+    filename = fullfile(save_folder, sprintf('frame%d.png', i));
+    if save_figures == 1
+        imwrite(cur_frame, filename);
     end
 
+    if ~isnan(prev_frame)
+        tic;
+        [motion_field, predicted_frame] = ICV_motionEstBM(prev_frame, ...
+            cur_frame, block_size, search_window);
+        toc;
+        
+%         filename = fullfile(save_folder, sprintf('frame%d-predicted.png', i));
+%         imwrite(predicted_frame, filename);
+
+        imshow(cur_frame);
+        hold on
+        quiver(motion_field(1, :, :), motion_field(2, :, :), ...
+               motion_field(3, :, :), motion_field(4, :, :), ...
+               0.5, 'color', [1 0 0]);
+        hold off
+        
+        frame = getframe(gcf, [92, 63, 352, 288]);
+        [frame_with_mf, ~] = frame2im(frame);
+        if save_figures == 1
+            filename = fullfile(save_folder, sprintf('frame%d-motion-field.png', i));
+            imwrite(frame_with_mf, filename);
+            filename = fullfile(save_folder, sprintf('frame%d-predicted.png', i));
+            imwrite(predicted_frame, filename);
+        end
+    end
     prev_frame = cur_frame;
     i = i + 1;
 end
